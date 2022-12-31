03f3e9f95abf01fbca28f914b928e2d6e2ada093
Ricky_Iwasaki <rickyiwasaki@gmail.com>
2022-12-31 06:56:34 -0800

    part 5.1

--- page.js
+++ page.js
@@ -77,6 +77,12 @@ addGameBtn.forEach((btn) => {
         if(players.length >= 2){
             const gameForm = document.querySelector('#game-form');
             toggleDisp(gameForm);
+
+            const selectNumPlayers = document.querySelector('#game-form-num-players');
+            selectNumPlayers.addEventListener('change', changePlayerSelect(selectNumPlayers.value));
+        }
+        else{
+            alert('please add players')
         }
     });
 });
@@ -103,23 +109,47 @@ formCover.addEventListener('click', () => {
     }
 });
 
+let headGame = null;
+const changePlayerSelect = (numPlayers) => {
+    for(let i = 0; i < numPlayers; i++){
+        const selectPlayers = document.querySelector('#game-form-select-players');
+        const newSelect = document.createElement('select');
+        selectPlayers.appendChild(newSelect);
+
+        newSelect.classList.add('p-5px');
+        newSelect.classList.add('border-0');
+        newSelect.classList.add('round-3px');
+        newSelect.classList.add('bg-1');
+        newSelect.classList.add('c-2');
+        newSelect.classList.add('box-shadow');
+        newSelect.classList.add('fs-15px');
+
+        players.forEach((player, i) => {
+            const option = document.createElement('option');
+            option.value = i;
+            option.innerText = player.name;
+            newSelect.appendChild(option);
+            //add style class
+        });
+
+        const gamePlayers = Array.from(selectPlayers.children).reduce((arr, p, j) => {
+            // if(selectPlayers.children[j].select === true){
+                arr[j] = selectPlayers.children[j].value
+            // }
+        }, []);
+
+        let temp = null;
+        if(headGame !== null){
+            temp = headGame;
+        }
+        headGame = new Node(new Game(7, 6, ...gamePlayers), null, temp);
+    }
+}
 const gameFormSubmitBtn = document.querySelector('#game-form button');
 gameFormSubmitBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    if(players.length >= 2){
-        const selectNumPlayers = document.querySelector('#game-form-num-players');
-        const numPlayers = selectNumPlayers.value;
-        for(let i = 0; i < numPlayers; i++){
-            const selectPlayers = document.querySelector('#game-form-select-players');
-            const newSelect = document.createElement('select');
-            // do this
-            // add num players of select, for each select have players.length of options
-            // add selected players to new Game();
-        }
-    }
-    else{
-        alert('please add players');
-    }
+    const selectNumPlayers = document.querySelector('#game-form-num-players');
+    const numPlayers = selectNumPlayers.value;
 });
 
 const addPlayer = (name, color) => {
@@ -196,6 +226,174 @@ playerFormSubmitBtn.addEventListener('click', (event) => {
 
 
 // game
+class Node{
+    constructor(GAME, prev, next){
+        this.GAME = GAME;
+        this.prev = prev;
+        this.next = next;
+    }
+}
+class Game{
+    constructor(WIDTH, HEIGHT, ...gamePlayers){
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+
+        this.gamePlayers = [...gamePlayers];
+        this.currPlayer = this.gamePlayers[Math.floor(Math.random() * this.gamePlayers.length)];
+
+        this.board = [];
+        this.table = document.createElement('table');
+        this.makeBoard();
+        this.makeHTMLBoard();
+    }
+    makeBoard(){
+        for(let y = 0; y < this.HEIGHT; y++){
+            this.board.push(Array.from( { length: this.WIDTH } ));
+        }
+    }
+    makeHTMLBoard(){
+        const games = document.querySelector('#games');
+        this.table.classList.add('p-2');
+        this.table.classList.add('m-2');
+        this.table.classList.add('float-left');
+        this.table.classList.add('bg-2');
+        games.insertBefore(this.table, games.firstChild);
+
+        const topTr = document.createElement('tr');
+        this.table.appendChild(topTr);
+        this.handleTopClick = this.handleTopClick.bind(this);
+        topTr.addEventListener('click', this.handleTopClick);
+        for(let x = 0; x < this.WIDTH; x++){
+            const topCell = document.createElement('td');
+            topCell.setAttribute('id', x);
+            topTr.appendChild(topCell);
+            topCell.classList.add('w-50px');
+            topCell.classList.add('h-50px');
+            topCell.classList.add('border-1');
+            topCell.classList.add('box-shadow-5');
+            topCell.classList.add('bga-3');
+        }
+
+        for(let y = 0; y < this.HEIGHT; y++){
+            const row = document.createElement('tr');
+            for(let x = 0; x < this.WIDTH; x++){
+                const cell = document.createElement('td');
+                cell.setAttribute('id', `${y}-${x}`);
+                row.appendChild(cell);
+                cell.classList.add('w-50px');
+                cell.classList.add('h-50px');
+                cell.classList.add('border-1');
+            }
+            this.table.appendChild(row);
+        }
+    }
+    findSpotForCol(x){
+        for (let y = this.HEIGHT - 1; y >= 0; y--) {
+            if (!this.board[y][x]) {
+              return y;
+            }
+          }
+          return null;
+    }
+    placeInTable(y, x){
+        const coin = document.createElement('div');
+        coin.classList.add('w-50px');
+        coin.classList.add('h-50px');
+        coin.classList.add('round-50');
+        coin.classList.add('pos-relative');
+        coin.style.backgroundColor = this.currPlayer.color;
+        coin.style.boxShadow = `${this.currPlayer.color} 0 0 5px 2px`;
+        newAnimation(`
+            @keyframes dropCoin{
+                from {top: ${-(y + 1) * 52}px;}
+                to {top: 0;}
+            }
+            .anima-1{
+                animation-name: dropCoin;
+                animation-duration: 1s;
+            }
+        `);
+        coin.classList.add('anima-1');
+        const topCell = this.table.querySelectorAll('tr')[y + 1].querySelectorAll('td')[x];
+        topCell.appendChild(coin);
+    }
+    endGame(){
+        this.table.parentElement.removeChild(this.table);
+        const footer = document.querySelector('#footer');
+        let currPlayerIndex;
+        Array.from(footer.children).some((div, i) => {
+            if(div.firstChild.textContent === this.currPlayer.name){
+                currPlayerIndex = i;
+                return true;
+            }
+            return false;
+        });
+        footer.children[currPlayerIndex].lastChild.textContent = ++players[currPlayerIndex].score;
+        alert('game finished');
+    }
+    handleTopClick(event){
+        const x = +event.target.id;
+        this.findSpotForCol = this.findSpotForCol.bind(this);
+        const y = this.findSpotForCol(x);
+        if(y === null){
+            return;
+        }
+        this.board[y][x] = this.currPlayer;
+        this.placeInTable(y, x);
+
+        if(this.checkForWin()){
+            return this.endGame();
+        }
+        if(this.board.every((row) => row.every(cell => cell))){
+            return this.endGame();
+        }
+
+        let nextPlayerIndex;
+        this.gamePlayers.some((player, i) => {
+            if(player === this.currPlayer){
+                nextPlayerIndex = i + 1;
+                return true;
+            }
+            return false;
+        });
+        if(nextPlayerIndex === this.gamePlayers.length){
+            this.currPlayer = this.gamePlayers[0];
+        }
+        else{
+            this.currPlayer = this.gamePlayers[nextPlayerIndex];
+        }
+    }
+    checkForWin(){
+        const _win = (cells) => cells.every(([y, x]) => 
+            y >= 0 &&
+            y < this.HEIGHT &&
+            x >= 0 &&
+            x < this.WIDTH &&
+            this.board[y][x] === this.currPlayer
+        );
+
+        for(let y = 0; y < this.HEIGHT; y++){
+            for(let x = 0; x < this.WIDTH; x++){
+                const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+                const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+                const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+                const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+                
+                if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+class Player{
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+}
+
+
 let players = [];
 
 
